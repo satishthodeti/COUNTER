b592710e171eae6e7fd46adb8aa1cd4f32f10e6b
satishthodeti <satishthodeti39gamail.com>
2023-04-24 11:43:35 +0000

    COUNTER-class

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,9 @@
+// import {Component} from 'react'
+
+// const Welcome extends Component{
+//  render() {
+//         const {name} = this.props
+//         return <h1>Hello, {name}</h1>
+//     }
+// }
+// export default Welcome
